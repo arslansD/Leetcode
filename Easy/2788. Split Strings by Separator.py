5a00eace3f003b3335b986d8ad9ee4516362781f
arslansD <55465158+arslansD@users.noreply.github.com>
2024-01-08 19:22:34 +0600

    New file

---
+++ Easy/2788. Split Strings by Separator.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
+        answ=[]
+        for word in words:
+            for w in word.split(separator):
+                if w:
+                    answ.append(w)
+        return answ
